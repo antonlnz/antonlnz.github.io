4705ff3d3c464382552e28d3e5e99e710d121df2
Anton <antonlopez2403@gmail.com>
2023-04-22 19:06:12 +0200

    Updates

---
+++ precache-manifest.3c41b66fcd684130123ad0a21e31a021.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "231dfce89d3481ab5ccef287e757665f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3751ca4698a403b56073",
+    "url": "/static/css/2.ad5ce37f.chunk.css"
+  },
+  {
+    "revision": "3b96690d29b43a094812",
+    "url": "/static/css/main.c63199a4.chunk.css"
+  },
+  {
+    "revision": "3751ca4698a403b56073",
+    "url": "/static/js/2.3c47fc72.chunk.js"
+  },
+  {
+    "revision": "b12aaea9c7d723083f2c844fa56b8dc2",
+    "url": "/static/js/2.3c47fc72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b96690d29b43a094812",
+    "url": "/static/js/main.0cac5ba2.chunk.js"
+  },
+  {
+    "revision": "c3f9020cd1650b649caa",
+    "url": "/static/js/runtime-main.b3ce09de.js"
+  }
+]);
